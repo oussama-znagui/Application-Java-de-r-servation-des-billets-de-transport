37b1755b408151cf83f5c8b0dd304e1c71769474
oussama <znagui.info@gmail.com>
2024-08-31 22:47:37 +0100

    create Enums & 2 models (Offer , Contract)

--- App/src/Model/Offer.java
+++ App/src/Model/Offer.java
@@ -29,4 +29,77 @@ public class Offer {
         this.discountValue = discountValue;
         this.contract = contract;
     }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOfferName() {
+        return offerName;
+    }
+    public void setOfferName(String offerName) {
+        this.offerName = offerName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public Discount getDiscountType() {
+        return discountType;
+    }
+    public void setDiscountType(Discount discountType) {
+        this.discountType = discountType;
+    }
+
+    public float getDiscountValue() {
+        return discountValue;
+    }
+    public void setDiscountValue(float discountValue) {
+        this.discountValue = discountValue;
+    }
+
+    public Contract getContract() {
+        return contract;
+    }
+    public void setContract(Contract contract) {
+        this.contract = contract;
+    }
+
+    public OfferStatus getStatus() {
+        return status;
+    }
+    public void setStatus(OfferStatus status) {
+        this.status = status;
+    }
+
+    public String getConditions() {
+        return conditions;
+    }
+    public void setConditions(String conditions) {
+        this.conditions = conditions;
+    }
+
+
+
 }
