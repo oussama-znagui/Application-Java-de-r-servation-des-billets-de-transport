59c81d2ca5fd8e05865158e765865e47dbf0a1db
oussama <znagui.info@gmail.com>
2024-09-03 15:48:57 +0100

    Contract Repository

--- App/src/Repository/Interface/ContractRepositoryInterface.java
+++ App/src/Repository/Interface/ContractRepositoryInterface.java
@@ -1,4 +1,9 @@
 package Repository.Interface;
 
+import Model.Contract;
+
+import java.sql.SQLException;
+
 public interface ContractRepositoryInterface {
+    public boolean addContract(Contract contract) throws SQLException;
 }
