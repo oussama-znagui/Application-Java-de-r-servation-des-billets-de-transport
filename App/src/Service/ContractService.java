e736cf00b8da518ffd3914207dc22e1b239ee107
oussama <znagui.info@gmail.com>
2024-09-04 12:11:29 +0100

    Contract repository&Service

---
+++ App/src/Service/ContractService.java
@@ -0,0 +1,16 @@
+package Service;
+
+import Model.Contract;
+import Repository.ContractRepository;
+
+import java.util.Scanner;
+
+public class ContractService {
+    public Scanner scanner = new Scanner(System.in);
+    public ContractRepository repository = new ContractRepository();
+
+
+    public Contract getData(){
+
+    }
+}
